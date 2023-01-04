02dfcb51922f5ddf1093a4069869875144534cf7
platosha-git <15platosha@gmail.com>
2023-01-04 13:53:46 +0300

    done rentals controllers

--- CarRentalSystem/Rentals/Controllers/RentalsAPIController.cs
+++ CarRentalSystem/Rentals/Controllers/RentalsAPIController.cs
@@ -40,7 +40,7 @@ namespace Rentals.Controllers
         {
             if (rental == null) return null;
             
-            RentalsDTO rentalDTO = new RentalsDTO()
+            var rentalDTO = new RentalsDTO()
             {
                 RentalUid = rental.RentalUid,
                 Username = rental.Username,
@@ -56,19 +56,19 @@ namespace Rentals.Controllers
 
         private List<RentalsDTO> ListRentalsDTO(List<Rental> lRentals)
         {
-            List<RentalsDTO> lRentalsDTO = new List<RentalsDTO>();
+            var lRentalsDTO = new List<RentalsDTO>();
             foreach (var rental in lRentals)
             {
-                RentalsDTO rentalDTO = InitRentalsDTO(rental);
+                var rentalDTO = InitRentalsDTO(rental);
                 lRentalsDTO.Add(rentalDTO);
             }
 
             return lRentalsDTO;
         }
 
-        /// <summary>Получить информацию о всех арендах пользователя</summary>
+        /// <summary> Получить информацию о всех арендах пользователя </summary>
         /// <param name="X-User-Name"> Имя пользователя </param>
-        /// <returns>Информация обо всех арендах</returns>
+        /// <returns> Информация обо всех арендах </returns>
         [HttpGet]
         [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(List<RentalsDTO>))]
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
@@ -82,13 +82,15 @@ namespace Rentals.Controllers
             }
             catch (Exception e)
             {
-                _logger.LogError(e, "+ Error occurred trying GetRentalsByUsername!");
+                _logger.LogError(e, "+RentalsAPI: Error while trying to GetRentalsByUsername");
                 throw;
             }
         }
-
-        // Glen
-        // 8b33afd0-9850-41c8-8325-32b5ea91759c
+        
+        /// <summary> Информация по конкретной аренде пользователя </summary>
+        /// <param name="rentalUid"> UUID аренды </param>
+        /// <param name="X-User-Name"> Имя пользователя </param>
+        /// <returns> Информация по конкретному бронированию </returns>
         [HttpGet("{rentalUid:guid}")]
         [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(RentalsDTO))]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
@@ -108,34 +110,63 @@ namespace Rentals.Controllers
             }
             catch (Exception e)
             {
-                _logger.LogError(e, "+ Error occurred trying GetRentalByRentalUid!");
+                _logger.LogError(e, "+RentalsAPI: Error while trying to GetRentalByUid");
                 throw;
             }
         }
 
+        /// <summary> Забронировать автомобиль </summary>
+        /// <param name="X-User-Name"> Имя пользователя </param>
+        /// <returns> Информация о бронировании авто </returns>
         [HttpPost]
         [ProducesResponseType(StatusCodes.Status201Created, Type = typeof(RentalsDTO))]
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         public async Task<IActionResult> CreateRental([FromBody] RentalsDTO rentalDTO)
         {
-            var rentalToAdd = GetRentalFromDTO(rentalDTO);
-            var addedRental = await _rentalsController.AddRental(rentalToAdd);
+            try
+            {
+                var rentalToAdd = GetRentalFromDTO(rentalDTO);
+                var addedRental = await _rentalsController.AddRental(rentalToAdd);
 
-            var response = InitRentalsDTO(addedRental);
-            return Created($"/api/v1/{addedRental.Id}", response);
+                var response = InitRentalsDTO(addedRental);
+                return Created($"/api/v1/{addedRental.Id}", response);
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, "+RentalsAPI: Error while trying to CreateRental");
+                throw;
+            }
         }
         
-        [HttpPatch("{username}/{rentalUid}/{status}")]
+        /// <summary> Завершение аренды автомобиля </summary>
+        /// <param name="rentalUid"> UUID аренды </param>
+        /// <param name="X-User-Name"> Имя пользователя </param>
+        /// <returns> Аренда успешно завершена </returns>
+        [HttpPatch("{username}/{rentalUid:guid}/{status}")]
         [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(RentalsDTO))]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
-        public async Task<IActionResult> FinishRent(string username, Guid rentalUid, string status)
+        public async Task<IActionResult> FinishRental(string username, Guid rentalUid, string status)
         {
-            var rental = await _rentalsController.GetRentalByRentalUid(username, rentalUid);
-            rental.Status = status;
-            await _rentalsController.FinishRent(rental);
+            try
+            {
+                var rental = await _rentalsController.GetRentalByRentalUid(username, rentalUid);
+                rental.Status = status;
+                await _rentalsController.FinishRental(rental);
 
-            var response = InitRentalsDTO(rental);
-            return Ok(response);
+                var response = InitRentalsDTO(rental);
+                return Ok(response);
+            }
+            catch (HttpRequestException e) when (e.StatusCode == HttpStatusCode.NotFound)
+            {
+                return NotFound(username);
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, "+RentalsAPI: Error while trying to FinishRental");
+                throw;
+            }
+            
         }
     }
 }
